6baf06769015684cf825cf95782debed7de6e4d9
treatmesubj <jrock4503@hotmail.com>
2024-05-13 17:19:05 -0500

    steal yaml.nvim

--- lua/jsonpath.lua
+++ lua/jsonpath.lua
@@ -25,12 +25,59 @@ local contains_special_characters = function(str)
   return str:match("[^a-zA-Z0-9_]")
 end
 
+-- plagiarized from https://github.com/cuducos/yaml.nvim/blob/main/lua/yaml_nvim/init.lua
+local function get_keys(root)
+  local keys = {}
+  for node, name in root:iter_children() do
+    if name == "key" then
+      table.insert(keys, node)
+    end
+
+    if node:child_count() > 0 then
+      for _, child in pairs(get_keys(node)) do
+        table.insert(keys, child)
+      end
+    end
+  end
+  return keys
+end
+
+-- plagiarized from https://github.com/cuducos/yaml.nvim/blob/main/lua/yaml_nvim/init.lua
+M.all_keys = function()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local ft = vim.api.nvim_buf_get_option(bufnr, "ft")
+  local tree = vim.treesitter.get_parser(bufnr, ft):parse()[1]
+  local root = tree:root()
+  return get_keys(root)
+end
+
+-- plagiarized from https://github.com/cuducos/yaml.nvim/blob/main/lua/yaml_nvim/init.lua
+M.get_key_relevant_to_cursor = function()
+  local cursor_line = vim.api.nvim_win_get_cursor(0)[1]
+  local previous_node = nil
+
+  for _, node in pairs(M.all_keys()) do
+    local node_line, _ = node:start()
+    node_line = node_line + 1
+
+    if cursor_line == node_line then
+      return node
+    end
+
+    if cursor_line < node_line then
+      return previous_node
+    end
+
+    previous_node = node
+  end
+end
+
 M.get = function()
   if not parsers.has_parser() then
     return ""
   end
 
-  local current_node = ts_utils.get_node_at_cursor()
+  local current_node = M.get_key_relevant_to_cursor()
   if not current_node then
     return ""
   end
